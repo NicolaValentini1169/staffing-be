b0ac53bdbdb21cd2019f6cc6f919c3dcef113b60
Nicola Valentini <valentini@omicronconsulting.it>
2021-12-21 13:08:19 +0100

    Add user delete method

--- src/main/java/com/my_virtual_space/staffing/security/services/UserService.java
+++ src/main/java/com/my_virtual_space/staffing/security/services/UserService.java
@@ -5,7 +5,9 @@ import com.my_virtual_space.staffing.security.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -23,6 +25,14 @@ public class UserService {
 
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+
+    public User deleteById(UUID id) {
+        return userRepository.findById(id).map(user -> {
+            userRepository.deleteById(id);
+
+            return user;
+        }).orElseThrow(() -> new EntityNotFoundException("Utente non trovato."));
     }
 
 }
